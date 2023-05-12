7cf667f325901c8eca9e36425f43d6da987d587e
adamb35 <113035019+adamb35@users.noreply.github.com>
2023-05-12 11:05:28 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -21,7 +21,7 @@ Papa.parse("questions.csv", {
           '<label><input type="radio" name="question' + i + '" value="B">' + questions[i].optionB + '</label>' +
           '<label><input type="radio" name="question' + i + '" value="C">' + questions[i].optionC + '</label>' +
           '<label><input type="radio" name="question' + i + '" value="D">' + questions[i].optionD + '</label>' +
-          '</div>'
+          '</div>'+'<br>'
         );
       }
       quizContainer.innerHTML = output.join('');
